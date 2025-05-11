92742be9ac7390a1c073c7055b37e55ac01340e7
Eric Passmore <eric.passmore@gmail.com>
2025-05-11 09:18:53 -0400

    refactored tests seperate eosio from vaulta

--- tests/functional/utils.py
+++ tests/functional/utils.py
@@ -1,7 +1,21 @@
 from os import listdir
 from pathlib import Path
 
+def tag_files(tag, directory):
+    return [(tag, f) for f in listdir(directory)]
+
 ROOT_SCREENSHOT_PATH = Path(__file__).parent.resolve()
 
 CORPUS_DIR = Path(__file__).parent.parent / "corpus"
-CORPUS_FILES = listdir(CORPUS_DIR)
+eosio_corpus_dir = CORPUS_DIR / "eosio"
+vaulta_corpus_dir = CORPUS_DIR / "vaulta"
+
+# List files in both directories
+eosio_tagged_corupus_files = tag_files("eosio", eosio_corpus_dir)
+vaulta_tagged_corupus_files = tag_files("vaulta", vaulta_corpus_dir)
+
+# Get all .json files in the directory (non-recursive)
+# note need saftey check when extracting, as first element is undefined 
+untagged_corpus_files = [(None, filename) for filename in list(CORPUS_DIR.glob("*.json"))]
+
+TAGGED_CORPUS_FILES = eosio_tagged_corupus_files + vaulta_tagged_corupus_files + untagged_corpus_files
